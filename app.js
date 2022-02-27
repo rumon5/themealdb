70b8b952d6675958e5823d9e0556853974feda9e
mdrumon42243 <mdrumonkhan48519@gmail.com>
2022-02-27 19:30:57 +0600

    Details section done

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ document.getElementById('search-button').addEventListener('click', function () {
 });
 
 const displayData = products => {
-    console.log(products);
     const cardContainer = document.getElementById('card-container');
     for (const product of products) {
         console.log(product)
@@ -21,10 +20,41 @@ const displayData = products => {
                 <div class="card-body">
                     <h5 class="card-title">${product.strMeal}</h5>
                     <p class="card-text">${product.strInstructions.slice(0, 150)}</p>
-                        <button type="button" class="btn btn-primary active">See details</button>
+                    <a href="#main-container" style="text-decoration: none;">
+                    <button type="button" onclick="getDetailsId(${product.idMeal})" class="btn btn-primary active">See
+                        details</button>
+                </a>
                 </div>
             </div>
         `;
         cardContainer.appendChild(div);
     }
+}
+
+const getDetailsId = id => {
+    const url = (`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayDetails(data.meals[0]))
+
+}
+const displayDetails = data => {
+    console.log(data)
+    const mainContainer = document.getElementById('main-container');
+    const div = document.createElement('div');
+    div.classList.add('bg-white')
+    div.classList.add('text-black')
+    div.classList.add('p-5')
+    div.classList.add('rounded')
+    div.innerHTML = `
+    <img class="rounded"  style="max-height: 70vh; max-width: 70vh;" src="${data.strMealThumb}" alt="">    
+
+    <h5 class="mt-2" >${data.strMeal}</h5>
+
+    <h5 class="mt-2 text-success" >${data.strArea}</h5>
+
+    <p>${data.strInstructions}</p>
+    
+    `;
+    mainContainer.appendChild(div);
 }
